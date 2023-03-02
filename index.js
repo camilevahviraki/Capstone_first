1726c08b835a835bce4b2e663783ec296808dc02
camilevahviraki <camilevahviraki@gmail.com>
2023-03-02 02:28:23 +0200

    refactor javascript

--- index.js
+++ index.js
@@ -2,230 +2,172 @@ const ButtonShowab = document.getElementById('hamburger1');
 const ButtonHideab = document.getElementById('hamburger2');
 const HomePage = document.getElementById('home_page');
 const AboutPage = document.getElementById('About_section');
+const logoN = document.getElementById('lodNav');
+const cardsContainerDesck =  document.getElementById('futuresDesck');
+const cardsContainerMobile = document.getElementById('futuresSp');
+const showMore = document.getElementById('showMore');
+const showLess = document.getElementById('showLess');
+const navLinksContainer = document.getElementById('Ul_desk');
+const navLinksMobile = document.getElementById('ul_nav');
+const body = document.getElementById('body');
+
+const navLinks = [
+  { text: 'Home', id: {desk: 'home2', mob: 'home'} },
+  { text: 'About', id: {desk: 'about2', mob: 'about'} },
+  { text: 'Join', id: {desk: 'join', mob: 'join'} },
+  { text: 'Program', id: {desk: 'Program', mob: 'Program'} },
+  // { text: 'News', id: {desk: 'News', mob: 'News'} },
+  { text: 'V Ignite', id: {desk: "Ignite", mob: ''} }
+];
+
+const presenters = [
+  {
+    picture: 'images/Craig_rowe.jpg',
+    name: 'Craig Rowe',
+    job: 'Director of Manufacturing and distribution',
+    story:
+      'To solve rural challenges around the world and increase connectivity,\
+      Craig Rowe launched Internet.org, a technology partnership that brings \
+      the Internet to remote and rural regions in the world.',
+  },
+  {
+    picture: 'images/Annaliese_Dayes.jpeg',
+    name: 'Annaliese Dayes',
+    job: 'Artistic director and management',
+    story:
+      'Annaliese Dayes did the study of public speaking in Artitic school in \
+      Sweden about 10 years ago. Girls were required to give speeches as part \
+      of their civic duties,',
+  },
+  {
+    picture: 'images/Square_Ophelia_Dennis.jpeg',
+    name: 'Ophelia Dennis',
+    job: 'Planning and Controlling director',
+    story:
+      'Ophelia Dennis ongoing learning has enabled her to embrace the broad \
+      scope the nursing profession',
+  },
+  {
+    picture: 'images/Alex_kramer.jpg',
+    name: 'Alex kramer',
+    job: 'Director Finance',
+    story:
+      'Undertaking an Australian School-based Apprenticeship helped Caitlin \
+      realise her dream carrer of becoming an ancounter',
+  },
+  {
+    picture: 'images/Charlie_brook.jpg',
+    name: 'Charlie Brook',
+    job: 'Technology and Engineering manager',
+    story:
+      'Charlie Brook managed to find a job where she combines her knoweldge \
+      of Electronic and Telecomunication with his love of environment',
+  },
+  {
+    picture: 'images/Chloe_everton.jpg',
+    name: 'Chloe Everton',
+    job: 'Organization director and manager',
+    story:
+      'Chloe Everton changed careers to management after studying in \
+      photgraphy and psychology.Shes now in the job of her childhood dream',
+  },
+];
+
+const displayNavLinks = (container, screen) => {
+  container.innerHTML = '';
+
+  navLinks.map((link) => {
+      container.innerHTML += `
+      <li>
+        <a class="links_Nav" id="${screen === 'desk'? link.id.desk: link.id.mob}" href="#">
+          ${link.text}
+        </a>
+      </li>`
+  })
+};
+
+displayNavLinks(navLinksMobile, 'mob');
+displayNavLinks(navLinksContainer, 'desk');
+
 const HomeBtn = document.getElementById('home');
 const AboutBtn = document.getElementById('about');
 const HomeBtn2 = document.getElementById('home2');
 const AboutBtn2 = document.getElementById('about2');
-const logoN = document.getElementById('lodNav');
 
 function showNav() {
   const UlNav = document.getElementById('ul_nav');
-
   document.getElementById('hamburger1').style.display = 'none';
   document.getElementById('hamburger2').style.display = 'flex';
+  body.style.overflow = 'hidden';
   UlNav.style.display = 'flex';
-  console.log('Nav shown');
 }
 function HideNav() {
   const UlNav = document.getElementById('ul_nav');
-
   document.getElementById('hamburger1').style.display = 'flex';
   document.getElementById('hamburger2').style.display = 'none';
+  body.style.overflow = 'scroll';
   UlNav.style.display = 'none';
-  console.log('Nav Hiden');
 }
 
 function showHomePage() {
   HomePage.style.display = 'flex';
   AboutPage.style.display = 'none';
-  console.log('show home page');
 }
 
 function showAboutPage() {
   HomePage.style.display = 'none';
   AboutPage.style.display = 'flex';
-  console.log('show About page');
 }
 
 ButtonShowab.addEventListener('click', showNav);
 ButtonHideab.addEventListener('click', HideNav);
-
-AboutBtn.addEventListener('click', HideNav);
-HomeBtn.addEventListener('click', HideNav);
-
-AboutBtn.addEventListener('click', showAboutPage);
-HomeBtn.addEventListener('click', showHomePage);
-
+AboutBtn.addEventListener('click', () => {showAboutPage(); HideNav()});
+HomeBtn.addEventListener('click', () => {showHomePage(); HideNav()});
 AboutBtn2.addEventListener('click', showAboutPage);
 HomeBtn2.addEventListener('click', showHomePage);
-
 logoN.addEventListener('click', showHomePage);
 
-function displayCard(nameC, jobC, pictureC, storieC, containerC) {
-  const containerCards = containerC;
-  const AllTags = [
-    {
-      type: 'div',
-      name: 'cardContainer',
-      class: 'feature_card',
-    },
-    {
-      type: 'div',
-      name: '2Images',
-      class: 'imagesP',
-    },
-    {
-      type: 'img',
-      name: 'chess',
-      class: 'chess',
-      src: 'images/Chess-PNG-Background.png',
-    },
-    {
-      type: 'img',
-      name: 'presenter',
-      class: 'presenter',
-      src: `${pictureC}`,
-    },
-    {
-      name: 'desc_presenter',
-      class: 'desc_presenter',
-      type: 'div',
-    },
-    {
-      name: 'name_presenter',
-      class: 'name_presenter',
-      type: 'h3',
-      texte: `${nameC}`,
-    },
-    {
-      name: 'function_presenter',
-      class: 'function_presenter',
-      type: 'h4',
-      texte: `${jobC}`,
-    },
-    {
-      name: 'line_grey',
-      class: 'line_grey',
-      type: 'div',
-    },
-    {
-      name: 'p_presenter',
-      class: 'p_presenter',
-      type: 'p',
-      texte: `${storieC}`,
-    },
-  ];
+const displayCards = (limit = presenters.length, container) => {
 
-  function createEveryTag() {
-    const output = [];
-    for (let i = 0; i < AllTags.length; i += 1) {
-      AllTags[i].name = document.createElement(AllTags[i].type);
-      AllTags[i].name.classList.add(AllTags[i].class);
-
-      if (Object.prototype.hasOwnProperty.call(AllTags[i], 'id')) {
-        AllTags[i].name.setAttribute('id', AllTags[i].id);
-      }
-
-      if (Object.prototype.hasOwnProperty.call(AllTags[i], 'src')) {
-        AllTags[i].name.src = AllTags[i].src;
-      }
-
-      if (Object.prototype.hasOwnProperty.call(AllTags[i], 'texte')) {
-        AllTags[i].name.innerText = AllTags[i].texte;
-      }
-
-      output.push(AllTags[i].name);
-    }
-
-    return output;
-  }
-  let myTags = {};
-  myTags = createEveryTag();
-
-  console.log(myTags);
-
-  myTags[0].append(myTags[1], myTags[4]);
-  myTags[1].append(myTags[2], myTags[3]);
-  myTags[4].append(myTags[5], myTags[6], myTags[7], myTags[8]);
-  containerCards.append(myTags[0]);
-
+  container.innerHTML = '';
+  
+  presenters.slice(0, limit).map((presenter) => {
+    container.innerHTML += `
+    <div class="feature_card">
+      <div class="imagesP">
+        <img class="chess" src="images/Chess-PNG-Background.png">
+        <img class="presenter" src="${presenter.picture}">
+      </div>
+      <div class="desc_presenter">
+        <h3 class="name_presenter">${presenter.name}</h3>
+        <h4 class="function_presenter">
+          ${presenter.job}
+        </h4>
+      <div class="line_grey">
+      </div>
+      <p class="p_presenter">
+        ${presenter.story}
+      </p>
+      </div>
+    </div>
+    `
+  })
 }
 
-function wrapCards() {
-  const presenters = [
-    {
-      picture: 'images/Craig_rowe.jpg',
-      name: 'Craig Rowe',
-      job: 'Director of Manufacturing and distribution',
-      storie:
-        'To solve rural challenges around the world and increase connectivity, Craig Rowe launched Internet.org, a technology partnership that brings the Internet to remote and rural regions in the world.',
-    },
-    {
-      picture: 'images/Annaliese_Dayes.jpeg',
-      name: 'Annaliese Dayes',
-      job: 'Artistic director and management',
-      storie:
-        'Annaliese Dayes did the study of public speaking in Artitic school in Sweden about 10 years ago. Girls were required to give speeches as part of their civic duties,',
-    },
-    {
-      picture: 'images/Square_Ophelia_Dennis.jpeg',
-      name: 'Ophelia Dennis',
-      job: 'Planning and Controlling director',
-      storie:
-        'Ophelia Dennis ongoing learning has enabled her to embrace the broad scope the nursing profession',
-    },
-    {
-      picture: 'images/Alex_kramer.jpg',
-      name: 'Alex kramer',
-      job: 'Director Finance',
-      storie:
-        'Undertaking an Australian School-based Apprenticeship helped Caitlin realise her dream carrer of becoming an ancounter',
-    },
-    {
-      picture: 'images/Charlie_brook.jpg',
-      name: 'Charlie Brook',
-      job: 'Technology and Engineering manager',
-      storie:
-        'Charlie Brook managed to find a job where she combines her knoweldge of Electronic and Telecomunication with his love of environment',
-    },
-    {
-      picture: 'images/Chloe_everton.jpg',
-      name: 'Chloe Everton',
-      job: 'Organization director and manager',
-      storie:
-        'Chloe Everton changed careers to management after studying in photgraphy and psychology.Shes now in the job of her childhood dream',
-    },
-  ];
+const displaySpeakersMobile = () => {
+  showMore.addEventListener('click', () => {
+    showLess.style.display = 'flex';
+    showMore.style.display = 'none';
+    displayCards(limit = presenters.length, cardsContainerMobile);
+  })
 
-  const showMore = document.getElementById('showMore');
-  const showMore2 = document.getElementById('showMore2');    
-
-  for (let i = 0; i < presenters.length; i += 1) {
-    const nameCard = presenters[i].name;
-    const jobCard = presenters[i].job;
-    const pictureCard = presenters[i].picture;
-    const storieCard = presenters[i].storie;
-
-    const section = document.querySelectorAll('.feature_card');
-    if (i >= 2) {
-      section[i].style.display = 'none';
-    }
-    showMore.addEventListener('click', function () {
-      showMore2.style.display = 'flex';
-      showMore.style.display = 'none';
-      section[i].style.display = 'flex';
-      console.log('clicked');
-    });
-
-    showMore2.addEventListener('click', function () {
-      if (i >= 2) {
-        section[i].style.display = 'none';
-        showMore2.style.display = 'none';
-        showMore.style.display = 'flex';
-        console.log('Hiden');
-      }
-    });
-
-    displayCard(nameCard, jobCard, pictureCard, storieCard, document.getElementById('futuresSp'));
-  }
-
-  for (let i = 0; i < presenters.length; i += 1) {
-    const nameCard = presenters[i].name;
-    const jobCard = presenters[i].job;
-    const pictureCard = presenters[i].picture;
-    const storieCard = presenters[i].storie;
-    displayCard(nameCard, jobCard, pictureCard, storieCard, document.getElementById('futuresDesck'));
-  }
-
+  showLess.addEventListener('click', () => {
+    showLess.style.display = 'none';
+    showMore.style.display = 'flex';
+    displayCards(limit = 2, cardsContainerMobile);
+  })
 }
-wrapCards();
+
+displayCards(limit = 2, cardsContainerMobile);
+displaySpeakersMobile();
+displayCards(limit = presenters.length, cardsContainerDesck);
